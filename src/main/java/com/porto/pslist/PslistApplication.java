5d12a716d779bafadbb557175b21bf200f349fe6
Jefferson da Silva Souza <jeffersonsilva.souza@portoseguro.com.br>
2023-05-10 17:19:14 -0300

    Initial Commit

---
+++ src/main/java/com/porto/pslist/PslistApplication.java
@@ -0,0 +1,13 @@
+package com.porto.pslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porto/pslist/PslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porto.pslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
