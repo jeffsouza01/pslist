0d45bb9b7ebe44bb82737bc9ff563fffe2c3423c
Jefferson da Silva Souza <jeffersonsilva.souza@portoseguro.com.br>
2023-05-10 22:33:04 -0300

    Include Domain model

--- src/main/java/com/porto/pslist/entities/GameList.java
+++ src/main/java/com/porto/pslist/entities/GameList.java
@@ -1,9 +1,15 @@
 package com.porto.pslist.entities;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_game_list")
 public class GameList {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
